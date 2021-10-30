4bdb224e6b7c80f02fb69dc5356ae5a653a501b1
toumasaya <luffy.w.pirate@gmail.com>
2021-10-31 03:22:35 +0800

    Add restart current scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     Rigidbody _rigidBody;
+    Scene _activeScene;
     [SerializeField] float _forwardSpeed = 10;
     [SerializeField] float _horizontalSpeed = 10;
 
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _activeScene = SceneManager.GetActiveScene();
     }
 
     void FixedUpdate() {
+        // Handle player gravity
         float _getVelocityY = _rigidBody.velocity.y;
+        // Input horizontal control
         float _controlHorizontalPos = Input.GetAxis("Horizontal") * _horizontalSpeed;
-
+        // Get Position Y
+        float _getPositionY = transform.position.y;
+        // Using Rigidbody.velocity make player move x position, go forward, and keep right gravity
         _rigidBody.velocity = new Vector3(_controlHorizontalPos, _getVelocityY, _forwardSpeed);
+
+        // If falling, restart the current scene
+        if (_getPositionY < -1) {
+            StartCoroutine(RestartCurrentLevel());
+        }
+    }
+
+    // If colliding enemy, restart current scene
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Enemy")) {
+            StartCoroutine(RestartCurrentLevel());
+        }
+    }
+
+    // Load current scene
+    void LoadCurrentScene () {
+        SceneManager.LoadScene(_activeScene.buildIndex);
+    }
+
+    IEnumerator RestartCurrentLevel () {
+        yield return new WaitForSeconds(1);
+        LoadCurrentScene();
     }
 }
